bb95af1eb36b4036cde975b2c9fac8888681f3bb
alastair-punler <alastair.punler@woodside.com.au>
2025-06-09 19:37:25 +0800

    prepared for railway

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template, request, jsonify
 import math
 import numpy as np
+import os
 
 app = Flask(__name__, static_url_path='/static', static_folder='static')
 
@@ -157,4 +158,5 @@ def calculate():
         }), 400
 
 if __name__ == '__main__':
-    app.run(debug=True) 
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port) 
